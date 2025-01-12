b70ef03695ce6badb4c2a0bfa5f90a4ec2246eb4
Rupasree Holegoudru <156749368+RupasreeH@users.noreply.github.com>
2025-01-11 23:06:56 -0600

    Update user.component.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,7 +13,7 @@ export class UserComponent {
 // imagePath = computed(()=>'assets/users/'+this.selectedUser().avatar)
 // @Input({required:true}) id!:string;
 // @Input({required:true}) avatar!:string;
-// @Input({required:true}) name!:string;
+// @Input({required:true}) name!:string;;
 @Input({required:true}) user!:User;
 @Input({required:true})selected!:boolean;
 @Output() select = new EventEmitter();
